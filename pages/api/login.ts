e3abae302014fb1f0c12db9f039754ec5e9981ba
Alan Munir Nege <allan.nege@gmail.com>
2022-10-07 11:36:18 -0300

    Configuração de banco de dados

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,16 +1,20 @@
 import type { NextApiRequest, NextApiResponse} from "next";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import type { respostaPadraoMsg } from "../../types/respostaPadraoMsg";
 
-export default (
+const endPointLogin = (
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<respostaPadraoMsg>
 ) =>{
     if(req.method === 'POST'){
         const {login, senha} = req.body;
         if(login === 'admin@admin.com' && 
            senha === 'Admin@123'){
-               res.status(200).json({msg: 'Usuario autenticado com sucesso'});
+               return res.status(200).json({msg: 'Usuario autenticado com sucesso'});
         }
         return res.status(405).json({erro: 'Usuario ou senha nao encontado'});
     }
     return res.status(405).json({erro: 'Metodo informado nao e valido'});
 }   
+
+export default conectarMongoDB(endPointLogin);
