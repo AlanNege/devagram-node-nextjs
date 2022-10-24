a94a69fda311319ddce909867473c89e92f22f66
Alan Munir Nege <allan.nege@gmail.com>
2022-10-24 08:46:43 -0300

    Pesquisa de usuario

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -4,27 +4,36 @@ import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
 import { usuarioModel } from "../../models/usuarioModel";
 import type { respostaPadraoMsg } from "../../types/respostaPadraoMsg"; 
 
-const pesquisaEndpoint = async (req: NextApiRequest, res: NextApiResponse<respostaPadraoMsg | any[]>) =>{
+const pesquisaEndpoint 
+    = async (req: NextApiRequest, res: NextApiResponse<respostaPadraoMsg | any[]>) =>{
     try{
         if(req.method === 'GET'){
-
-            const {filtro} = req.query;
-            if(!filtro || filtro.length <2){
-                return res.status(400).json({erro:'Favor informar pelo menos 2 caracteres para a busca'});
-            }
-
-            const usuariosEncontrados = await usuarioModel.find({
-                $or: [{ nome : {$regex:filtro, $options:'i'}},
+            if(req?.query?.id){
+                const usuarioEncontrado = await usuarioModel.findById(req?.query?.id);
+                if(!usuarioEncontrado){
+                    return res.status(400).json({erro:'Usuario nao encontrado'});
+                }
+                usuarioEncontrado.senha = null;
+                return res.status(200).json(usuarioEncontrado);
+            }else{
+                const {filtro} = req.query;
+                if(!filtro || filtro.length <2){
+                    return res.status(400).json({erro:'Favor informar pelo menos 2 caracteres para a busca'});
+                }        
+            
+                const usuariosEncontrados = await usuarioModel.find({
+                    $or: [{ nome : {$regex:filtro, $options:'i'}},
                    //{ email: {$regex:filtro, $options:'i'}}
-                ]   
-            });
+                ]  
+            }); 
+                
             return res.status(200).json(usuariosEncontrados);
-        }
-        return res.status(405).json({erro:'Metodo informado nao e valido'});
+        }};
+        return res.status(200).json({erro:'Metodo informado nao e valido'});
     }catch(e){
         console.log(e);
         return res.status(500).json({erro:'Nao foi possivel buscar usuario:'+e});
     }
+
 }
- 
 export default validarTokenJWT(conectarMongoDB(pesquisaEndpoint));
