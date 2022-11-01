c70369744ed4ee281661e513ebfd79ebfcb455ed
Alan Munir Nege <allan.nege@gmail.com>
2022-11-01 11:55:58 -0300

    Conclusao api de comentarios

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -3,6 +3,7 @@ import type {respostaPadraoMsg} from '../../types/respostaPadraoMsg';
 import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
 import {conectarMongoDB}  from '../../middlewares/conectarMongoDB';
 import { UsuarioModel } from "../../models/UsuarioModel";
+import { SeguidorModel } from "../../models/SeguidorModel";
 import {PublicacaoModel} from '../../models/PublicacaoModel';
 import { politicaCORS } from "../../middlewares/politicaCORS";
 
@@ -19,6 +20,41 @@ const feedEndpoint = async (req : NextApiRequest, res: NextApiResponse<respostaP
                     .find({idUsuario : usuario._id})
                     .sort({data : -1});  
                 return res.status(200).json (publicacoes);
+            }else{
+
+                const {userId} = req.query;
+                const usuarioLogado = await UsuarioModel.findById(userId);
+                if(!usuarioLogado){
+                    return res.status(400).json({erro:'Usuario nao encontrado'});
+                    
+                }
+
+                const seguidores = await SeguidorModel.find({usuarioId: usuarioLogado._id})
+                const seguidoresIds = seguidores.map(s => s.usuarioSeguidoId);
+
+                const publicacoes = await PublicacaoModel.find({
+                    $or : [
+                        {idUsuario : usuarioLogado._id},
+                        {idUsuario : seguidoresIds}
+                    ]
+                })
+                .sort({data: -1});
+
+                const result = [];
+                for (const publicacao of publicacoes){
+                    const usuarioDaPublicacao = await UsuarioModel.findById(publicacao.idUsuario);
+                    if(usuarioDaPublicacao){
+                        const final = {...publicacao._doc, usuario:{
+                            nome: usuarioDaPublicacao.nome,
+                            avatar: usuarioDaPublicacao.avatar
+                        }};
+                        result.push(final);
+                    }
+                }
+
+                return res.status(200).json(publicacoes)
+
+
             }
             
         }
